41dc4d9701a3ab808d81c998d2bb202f05bba7b2
YOUR NAME <ameya.charnalia@gmail.com>
2022-09-27 07:39:03 -0400

    Fixed update for when no government bills are being debated.

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ function getGovernmentBills() {
     return axios.get(url)
 } 
 
+
     new CronJob(
-        '0 7 * * *', // everyday at 7 am
+        '* * * * *', // everyday at 7 am
         function() {
             // Call Twitter BOT to post new Tweet
             getGovernmentBills()
@@ -21,9 +22,14 @@ function getGovernmentBills() {
                         postTweet(tweetText)
                     } else {
                         const governmentBills = allBills.filter(bill => bill.IsGovernmentBill)
-                        const formattedBills = governmentBills.map(bill => `${bill.NumberCode}: ${bill.StatusName}.`)
-                        const tweetText = `${date}\n${formattedBills.join('\r\n')}\nMore information: https://www.parl.ca/legisinfo/`
-                        postTweet(tweetText)
+                        if(governmentBills.length === 0) {
+                            const tweetText = `${date}\nNo government bills being debated today.`
+                            postTweet(tweetText)
+                        } else {
+                            const formattedBills = governmentBills.map(bill => `${bill.NumberCode}: ${bill.StatusName}.`)
+                            const tweetText = `${date}\n${formattedBills.join('\r\n')}\nMore information: https://www.parl.ca/legisinfo/`
+                            postTweet(tweetText)
+                        }
                     }
                 }).catch(err => {
                     postTweet("Having technical difficulties.")
