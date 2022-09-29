5e35fb855ad9123a9dd50ae6dd1eb0982b1427ea
YOUR NAME <ameya.charnalia@gmail.com>
2022-09-29 08:09:44 -0400

    Fixed invalid date

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ function getGovernmentBills() {
     return axios.get(url)
 } 
 
+
+
     new CronJob(
         '0 7 * * *', // everyday at 7 am
         function() {
@@ -16,16 +18,16 @@ function getGovernmentBills() {
                 .then(response => {
                     const allBills = response.data
                     if(allBills.length === 0) {
-                        const tweetText = `${new Date('en-GB').toLocaleDateString()}\nParliament is not sitting today.\nMore information: https://www.parl.ca/legisinfo/`
+                        const tweetText = `${new Date().toLocaleDateString('en-GB')}\nParliament is not sitting today.\nMore information: https://www.parl.ca/legisinfo/`
                         postTweet(tweetText)
                     } else {
                         const governmentBills = allBills.filter(bill => bill.IsGovernmentBill)
                         if(governmentBills.length === 0) {
-                            const tweetText = `${new Date('en-GB').toLocaleDateString()}\nNo government bills being debated today.\nMore information: https://www.parl.ca/legisinfo/`
+                            const tweetText = `${new Date().toLocaleDateString('en-GB')}\nNo government bills being debated today.\nMore information: https://www.parl.ca/legisinfo/`
                             postTweet(tweetText)
                         } else {
                             const formattedBills = governmentBills.map(bill => `${bill.NumberCode}: ${bill.StatusName}.`)
-                            const tweetText = `${new Date('en-GB').toLocaleDateString()}\n${formattedBills.join('\r\n')}\nMore information: https://www.parl.ca/legisinfo/`
+                            const tweetText = `${new Date().toLocaleDateString('en-GB')}\n${formattedBills.join('\r\n')}\nMore information: https://www.parl.ca/legisinfo/`
                             postTweet(tweetText)
                         }
                     }
