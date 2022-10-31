a2c9fd8d3983552b9213b3638f9d71dd14419563
YOUR NAME <ameya.charnalia@gmail.com>
2022-10-31 19:05:44 -0400

    Added short title of bill

--- app.js
+++ app.js
@@ -26,7 +26,27 @@ function getGovernmentBills() {
                             const tweetText = `${new Date().toLocaleDateString('en-GB')}\nNo government bills being debated today.\nMore information: https://www.parl.ca/legisinfo/`
                             postTweet(tweetText)
                         } else {
-                            const formattedBills = governmentBills.map(bill => `${bill.NumberCode}: ${bill.StatusName}.`)
+                            // const formattedBills = governmentBills.map(bill => `${bill.NumberCode}: ${bill.StatusName}.`)
+                            function checkStatus(status) {
+                                if(status === 'At first reading in the House of Commons') {
+                                    return '1st reading in @HoCChamber'
+                                } else if(status === 'At second reading in the House of Commons') {
+                                    return '2nd reading in @HoCChamber'
+                                } else if(status === 'At third reading in the House of Commons') {
+                                    return '2nd reading in @HoCChamber'
+                                } else if(status === 'At first reading in the Senate') {
+                                    return '1st reading in @SenateCA'
+                                } else if(status === 'At second reading in the Senate') {
+                                    return '2nd reading in @SenateCA'
+                                } else if(status === 'At third reading in the Senate') {
+                                    return '3rd reading in @SenateCA'
+                                } else if (status === 'At report stage in the House of Commons') {
+                                    return 'Report stage in @HoCChamber'
+                                } else if(status === 'At report stage in the Senate') {
+                                    return 'Report stage in @SenateCA'
+                                }
+                            }
+                            const formattedBills = governmentBills.map(bill => `${bill.NumberCode} - ${bill.ShortTitle}: ${checkStatus(bill.StatusName)}.`)
                             const tweetText = `${new Date().toLocaleDateString('en-GB')}\n${formattedBills.join('\r\n')}\nMore information: https://www.parl.ca/legisinfo/`
                             postTweet(tweetText)
                         }
