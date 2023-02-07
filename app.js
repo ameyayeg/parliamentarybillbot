e65d57e29b19da169815fb8356c611127d6d3db4
YOUR NAME <ameya.charnalia@gmail.com>
2023-02-07 08:12:20 -0500

    Added URL

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
-const CronJob = require('cron').CronJob;
+const CronJob = require('cron').CronJob
 const axios = require('axios')
-const { tweetThread } = require('./bot');
-
+const { tweetThread } = require('./bot')
 
 const url = `https://www.parl.ca/legisinfo/en/overview/json/onagenda`
 
 function getGovernmentBills() {
-    return axios.get(url)
-} 
+  return axios.get(url)
+}
 
-    new CronJob(
-        '0 7 * * *', // everyday at 7 am
-        function() {
-            // Call Twitter BOT to post new Tweet
-            getGovernmentBills()
-                .then(response => {
-                    const allBills = response.data
-                    if(allBills.length === 0) {
-                        const tweetText = `${new Date().toLocaleDateString('en-GB')}\nParliament is not sitting today.\nMore information: https://www.parl.ca/legisinfo/\n#cdnpoli`
-                        tweetThread(tweetText)
-                    } else {
-                        const governmentBills = allBills.filter(bill => bill.IsGovernmentBill)
-                        if(governmentBills.length === 0) {
-                            const tweetText = `${new Date().toLocaleDateString('en-GB')}\nNo government bills being debated today.\nMore information: https://www.parl.ca/legisinfo/\n#cdnpoli`
-                            tweetThread(tweetText)
-                        } else {
-                            const formattedBills = governmentBills.map(bill => `${bill.NumberCode} - ${bill.LongTitle}: ${bill.StatusName}.`)
-                            const tweetText = `${new Date().toLocaleDateString('en-GB')}\n${formattedBills.join('\r\n')}\n#cdnpoli`
-                            tweetThread(tweetText)
-                        }
-                    }
-                }).catch(err => {
-                    tweetThread("Having technical difficulties.")
-                })
-        },
-        null,
-        true,
-        'America/Toronto'
-    );
-
- 
+new CronJob(
+  '0 7 * * *', // everyday at 7 am
+  function () {
+    // Call Twitter BOT to post new Tweet
+    getGovernmentBills()
+      .then((response) => {
+        const allBills = response.data
+        if (allBills.length === 0) {
+          const tweetText = `${new Date().toLocaleDateString(
+            'en-GB'
+          )}\nParliament is not sitting today.\nMore information: https://www.parl.ca/legisinfo/\n#cdnpoli`
+          tweetThread(tweetText)
+        } else {
+          const governmentBills = allBills.filter(
+            (bill) => bill.IsGovernmentBill
+          )
+          if (governmentBills.length === 0) {
+            const tweetText = `${new Date().toLocaleDateString(
+              'en-GB'
+            )}\nNo government bills being debated today.\nMore information: https://www.parl.ca/legisinfo/\n#cdnpoli`
+            tweetThread(tweetText)
+          } else {
+            const formattedBills = governmentBills.map(
+              (bill) =>
+                `${bill.NumberCode} - ${bill.LongTitle}(http://www.parl.ca/legisinfo/en/bill/${bill.ParliamentNumber}-${bill.SessionNumber}/${bill.NumberCode}): ${bill.StatusName}.`
+            )
+            const tweetText = `${new Date().toLocaleDateString(
+              'en-GB'
+            )}\n${formattedBills.join('\r\n')}\n#cdnpoli`
+            tweetThread(tweetText)
+          }
+        }
+      })
+      .catch((err) => {
+        tweetThread('Having technical difficulties.')
+      })
+  },
+  null,
+  true,
+  'America/Toronto'
+)
